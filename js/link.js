208527a03093d34c78773edae8e7eac8c96609aa
Chenhao Xue <xch927027@pku.edu.cn>
2022-05-25 16:19:37 +0800

    video card title

--- js/link.js
+++ js/link.js
@@ -1,9 +1,11 @@
 // 链接的动画效果
 // 直接使用pili-link类，就能添加这一部分动效
-$(".pili-link").mouseover(function () {
+$(".pili-link").mouseenter(function () {
   $(this).css("transition", "color 0.3s");
-  $(this).attr("backupColor", $(this).css("color"));
+  if ($(this).attr("backupColor") == undefined)
+    $(this).attr("backupColor", $(this).css("color"));
   $(this).css("color", "#30c2dc");
+  $(this).css("cursor", "pointer");
 });
 
 $(".pili-link").mouseleave(function () {

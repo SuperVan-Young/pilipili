06a87a1c218d0bf69121f3aedd645139c886bcbf
fitzwong <owowoowang@pku.edu.cn>
2022-05-26 16:27:44 +0800

    new file:   script/header.js

---
+++ script/header.js
@@ -0,0 +1,8 @@
+$(".search-history").hide();
+// $(document).click(function(){
+//     $(".search-history").hide();
+// });
+$(".search-form").click(function(){
+    $(".search-history").show();
+
+});

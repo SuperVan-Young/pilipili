ef928bb729ea02a9196fa8f9e04746d24702acd3
fitzwong <owowoowang@pku.edu.cn>
2022-05-27 12:35:29 +0800

    modified:   css/header.css 	new file:   img/manhua_1.jpeg 	new file:   img/youxi_1.jpeg 	new file:   img/youxi_2.png 	new file:   img/zhibo_1.jpeg 	new file:   img/zhibo_7.jpeg 	modified:   index.html 	modified:   script/header.js

--- script/header.js
+++ script/header.js
@@ -1,8 +1,29 @@
 $(".search-history").hide();
-// $(document).click(function(){
-//     $(".search-history").hide();
-// });
-$(".search-form").click(function(){
+$(".search-bar").click(function(){
+    event.stopPropagation()
     $(".search-history").show();
-
 });
+$(document).click(function(){
+    $(".search-history").hide();
+});
+
+$(".zhibo-hover-div").hide();
+$(".header-zhibo").on("mouseenter", function(){
+    $(".zhibo-hover-div").animate({height: 'toggle'}, "fast");
+}).on("mouseleave", function(){
+    $(".zhibo-hover-div").animate({height: 'toggle'}, "fast");
+})
+
+$(".youxi-hover-div").hide();
+$(".header-youxi").on("mouseenter", function(){
+    $(".youxi-hover-div").animate({height: 'toggle'}, "fast");
+}).on("mouseleave", function(){
+    $(".youxi-hover-div").animate({height: 'toggle'}, "fast");
+})
+
+$(".manhua-hover-div").hide();
+$(".header-manhua").on("mouseenter", function(){
+    $(".manhua-hover-div").animate({height: 'toggle'}, "fast");
+}).on("mouseleave", function(){
+    $(".manhua-hover-div").animate({height: 'toggle'}, "fast");
+})

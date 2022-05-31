059cbff11e5a095fd759381aabee5ba20bb7e0b6
fitzwong <owowoowang@pku.edu.cn>
2022-05-31 11:18:30 +0800

    modified:   css/header.css 	modified:   index.html 	modified:   script/header.js

--- script/header.js
+++ script/header.js
@@ -16,46 +16,39 @@ $(document).click(function(){
 
 $(".zhibo-hover-div").hide();
 $(".header-zhibo").on("mouseenter", function(){
-    $(".zhibo-hover-div").animate({height: 'toggle'}, 100);
+    $(".zhibo-hover-div").stop().animate({height: 'toggle'}, 200);
 }).on("mouseleave", function(){
-    $(".zhibo-hover-div").animate({height: 'toggle'}, 100);
+    $(".zhibo-hover-div").stop().animate({height: 'toggle'}, 200);
 })
 
 $(".youxi-hover-div").hide();
 $(".header-youxi").on("mouseenter", function(){
-    $(".youxi-hover-div").animate({height: 'toggle'}, 100);
+    $(".youxi-hover-div").stop().animate({height: 'toggle'}, 200);
 }).on("mouseleave", function(){
-    $(".youxi-hover-div").animate({height: 'toggle'}, 100);
+    $(".youxi-hover-div").stop().animate({height: 'toggle'}, 200);
 })
 
 $(".manhua-hover-div").hide();
 $(".header-manhua").on("mouseenter", function(){
-    $(".manhua-hover-div").animate({height: 'toggle'}, 100);
+    $(".manhua-hover-div").stop().animate({height: 'toggle'}, 200);
 }).on("mouseleave", function(){
-    $(".manhua-hover-div").animate({height: 'toggle'}, 100);
+    $(".manhua-hover-div").stop().animate({height: 'toggle'}, 200);
 })
-
-// $(".xiazai-hover-div").hide();
-// $(".header-xiazai").on("mouseenter", function(){
-//     $(".xiazai-hover-div").animate({height: 'toggle'}, "fast");
-// }).on("mouseleave", function(){
-//     $(".xiazai-hover-div").animate({height: 'toggle'}, "fast");
-// })
 
 $(".user-hover-div").hide();
 $(".header-user").on("mouseenter", function(){
     // $(".right-header").animate({marginLeft: "0px"});
-    $(".user-hover-div").animate({height: 'toggle', top: "80px", left: "-105px"}, 100);
-    $(".user-head").animate({width: "82px", marginTop: "60px"}, "fast");
+    $(".user-hover-div").stop().animate({height: 'toggle', top: "80px", left: "-105px"}, 200);
+    $(".user-head").stop().animate({width: "82px", marginTop: "60px"}, "fast");
 }).on("mouseleave", function(){
     // $(".right-header").animate({marginLeft: "40px"});
-    $(".user-hover-div").animate({height: 'toggle', top: "40px", left: "-125px"}, 100);
-    $(".user-head").animate({width: "40px", marginTop: "0px"}, "fast");
+    $(".user-hover-div").stop().animate({height: 'toggle', top: "40px", left: "-125px"}, 200);
+    $(".user-head").stop().animate({width: "40px", marginTop: "0px"}, "fast");
 })
 
 $(".xiaoxi-hover-div").hide();
 $(".header-xiaoxi").on("mouseenter", function(){
-    $(".xiaoxi-hover-div").animate({height: 'toggle'}, 100);
+    $(".xiaoxi-hover-div").stop().animate({height: 'toggle'}, 200);
 }).on("mouseleave", function(){
-    $(".xiaoxi-hover-div").animate({height: 'toggle'}, 100);
+    $(".xiaoxi-hover-div").stop().animate({height: 'toggle'}, 200);
 })
